ec986ee6af125fdb913dd79fbc5b3736c823dd9c
Cristian Medina Peña <medinacristian@gmail.com>
2017-04-16 23:12:39 -0300

    Another solution to solve de problem "DiagonalDifference"

--- JAVA/src/Algorithms/Warmup/DiagonalDifference.java
+++ JAVA/src/Algorithms/Warmup/DiagonalDifference.java
@@ -8,6 +8,7 @@ import java.util.*;
  */
 public class DiagonalDifference {
     
+    /*
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
@@ -34,6 +35,29 @@ public class DiagonalDifference {
         
         System.out.println(Math.abs(pri-sec));
         
+    }*/
+    
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int a[][] = new int[n][n];
+                
+        for(int a_i=0; a_i < n; a_i++){
+            for(int a_j=0; a_j < n; a_j++){
+                a[a_i][a_j] = in.nextInt();
+            }
+        }
+        
+        int pri=0, sec=0;
+        for(int i=0, k=n-1; i<n; i++, k--){
+            for(int j=0; j<n; j++){
+                if(i==j){
+                    pri+=a[i][j];
+                }
+            }
+            sec+=a[i][k];
+        }
+        System.out.println(Math.abs(pri-sec));
     }
     
     
--- PHP/Algorithms/Warmup/DiagonalDifference.php
+++ PHP/Algorithms/Warmup/DiagonalDifference.php
@@ -1,5 +1,6 @@
 <?php
 
+/*
 $handle = fopen ("php://stdin","r");
 fscanf($handle,"%d",$n);
 $a = array();
@@ -27,5 +28,31 @@ for($i=0; $i<$n; $i++){
 }
 
 echo abs($pri-$sec);
+*/
+
+
+$handle = fopen ("php://stdin","r");
+fscanf($handle,"%d",$n);
+$a = array();
+for($a_i = 0; $a_i < $n; $a_i++) {
+   $a_temp = fgets($handle);
+   $a[] = explode(" ",$a_temp);
+   array_walk($a[$a_i],'intval');
+}
+
+$pri=0;
+$sec=0;
+
+for($i=0, $k=$n-1; $i<$n; $i++, $k--){
+    for($j=0; $j<$n; $j++){
+        if($i==$j){
+            $pri+=$a[$i][$j];
+        }
+    }
+    $sec+=$a[$i][$k];
+}
+
+echo abs($pri-$sec);
+
 
 ?>
