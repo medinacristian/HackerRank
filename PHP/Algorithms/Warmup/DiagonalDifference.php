a6575421abfc4124f28837a949727876191555f7
Cristian Medina Peña <medinacristian@gmail.com>
2017-04-16 22:55:39 -0300

    My solution to Diagonal Difference Problem.

---
+++ PHP/Algorithms/Warmup/DiagonalDifference.php
@@ -0,0 +1,31 @@
+<?php
+
+$handle = fopen ("php://stdin","r");
+fscanf($handle,"%d",$n);
+$a = array();
+for($a_i = 0; $a_i < $n; $a_i++) {
+   $a_temp = fgets($handle);
+   $a[] = explode(" ",$a_temp);
+   array_walk($a[$a_i],'intval');
+}
+
+//print_r($a);
+$pri=0;
+$sec=0;
+$cont=$n-1;
+
+for($i=0; $i<$n; $i++){
+    for($j=0; $j<$n; $j++){
+        if($i==$j){
+            $pri+=$a[$i][$j];
+        }
+        if($j==$cont){
+            $sec+=$a[$i][$j];
+            $cont--;
+        }
+    }
+}
+
+echo abs($pri-$sec);
+
+?>
