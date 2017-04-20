66b02b62d1f88a662d5674dcd8a707aa0c21a04f
Cristian Medina Peña <medinacristian@gmail.com>
2017-04-20 14:16:52 -0300

    Day 7

---
+++ JAVA/src/DaysOfCode30/Arrays.java
@@ -0,0 +1,24 @@
+package DaysOfCode30;
+
+import java.util.*;
+
+/**
+ *
+ * @author Cristian
+ */
+public class Arrays {
+    
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i < n; i++){
+            arr[i] = in.nextInt();
+        }
+        in.close();
+        for(int j=arr.length-1; j>=0; j--){
+            System.out.print(arr[j]+" ");
+        }
+    }
+    
+}
