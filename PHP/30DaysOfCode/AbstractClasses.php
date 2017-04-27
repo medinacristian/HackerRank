46b32821418aae0cbe9926bff7f06bb1ebcebf38
Cristian Medina Peña <medinacristian@gmail.com>
2017-04-27 02:00:15 -0300

    Day 13

---
+++ PHP/30DaysOfCode/AbstractClasses.php
@@ -0,0 +1,36 @@
+<?php
+abstract class Book
+{
+    
+    protected $title;
+    protected  $author;
+    
+     function __construct($t,$a){
+        $this->title=$t;
+        $this->author=$a;
+    }
+    abstract protected function display();
+}
+
+class MyBook extends Book{
+    private $price;
+    function __construct($title, $author, $price){
+        parent::__construct($title, $author);
+        $this->price = $price;
+    }
+    
+    function display(){
+        echo "Title: ".$this->title;
+        echo "Author: ".$this->author;
+        echo "Price: ".$this->price;
+    }
+    
+}
+
+$title=fgets(STDIN);
+$author=fgets(STDIN);
+$price=intval(fgets(STDIN));
+$novel=new MyBook($title,$author,$price);
+$novel->display();
+
+?>
