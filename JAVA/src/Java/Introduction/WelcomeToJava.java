842878286ed3a3d7c4e09fa943b90ebd88dd5bbe
Cristian Medina Peña <medinacristian@gmail.com>
2017-05-02 03:07:41 -0300

    Welcome to Java Solution

---
+++ JAVA/src/Java/Introduction/WelcomeToJava.java
@@ -0,0 +1,13 @@
+package Java.Introduction;
+
+/**
+ *
+ * @author Cristian
+ */
+public class WelcomeToJava {
+    public static void main(String[] args) {
+        /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
